b3f54fefcccc4a32afbe8a43c2dd49b4e0bfc2d4
Manavendra Sen <manavendra4288@gmail.com>
2022-04-10 01:16:08 +0530

    style: login

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,93 +1,113 @@
-import { ThemeProvider } from "@emotion/react";
-import { Avatar, Box, Button, createTheme, CssBaseline, Grid, Link, Paper, Typography } from "@mui/material";
-import TwitterIcon from '@mui/icons-material/Twitter';
+import {
+  Avatar,
+  Box,
+  Button,
+  createTheme,
+  CssBaseline,
+  Grid,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import React, { useState } from "react";
-import { Twittertheme } from "../../Theme/Twitter.theme";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { supabase } from "../../supabaseClient";
+import { APP_HOME } from "../../constants/routes";
 
 interface LoginProps {}
 
-const theme = createTheme();
-
-export const Login: React.FC<LoginProps> = ({}) => {
-
-  const [loading,setLoading] = useState(false);
+export const Login: React.FC<LoginProps> = () => {
+  // const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleTwitterLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try{
+    try {
       setLoading(true);
       const { error } = await supabase.auth.signIn({
-        provider: 'twitter',
+        provider: "twitter",
       });
-      if(error) throw error;
-    }catch(error:any){
-      console.log(error.message);
-    }finally{
-      setLoading(false);
-    }
-  }
 
+      if (error) throw error;
+    } catch (error: any) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+      // navigate(APP_HOME);
+    }
+  };
 
   return (
-      <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+    <Grid container component='main' sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            my: 20,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 20,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+        >
+          <Avatar
+            sizes='large'
+            sx={{ m: 4, bgcolor: "secondary.main", width: 80, height: 80 }}
           >
-            <Avatar sizes="large" sx={{ m: 4, bgcolor: 'secondary.main', width:80, height:80}}>
-              <AccountCircleIcon sx={{ width:100, height:100}}/>
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              SignIn
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleTwitterLogin} sx={{ mt: 1 }}>
-              <ThemeProvider theme={Twittertheme}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  color="primary"
-                  variant="contained"
-                  sx={{ mt: 4, mb: 2 }}
-                >
-                  {loading ? 'Loading...' : <>LogIn thorugh Twitter<TwitterIcon sx={{m:.5}}/></>}
-                </Button>
-              </ThemeProvider>
-              <Grid container>
-                <Grid item>
-                  <Link sx={{m:3}} href="https://twitter.com/" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+            <AccountCircleIcon sx={{ width: 100, height: 100 }} />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            SignIn
+          </Typography>
+          <Box
+            component='form'
+            noValidate
+            onSubmit={handleTwitterLogin}
+            sx={{ mt: 1 }}
+          >
+            <Button
+              type='submit'
+              fullWidth
+              color='primary'
+              variant='contained'
+              sx={{ mt: 4, mb: 2 }}
+            >
+              {loading ? (
+                "Loading..."
+              ) : (
+                <>
+                  LogIn thorugh Twitter
+                  <TwitterIcon sx={{ m: 0.5 }} />
+                </>
+              )}
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link sx={{ m: 3 }} href='https://twitter.com/' variant='body2'>
+                  {"Don't have an account? Sign Up"}
+                </Link>
               </Grid>
-            </Box>
+            </Grid>
           </Box>
-        </Grid>
+        </Box>
       </Grid>
-    </ThemeProvider>
+    </Grid>
   );
-
 };
