0da8e7ef6a3daf4e656640620a5c99d76574ce56
Manavendra Sen <manavendra4288@gmail.com>
2022-04-09 12:43:46 +0530

    feat: material ui setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>Hello World</h1>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/theme.ts
@@ -0,0 +1,82 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#06113C",
+    },
+    secondary: {
+      main: "#FF7761",
+    },
+    success: {
+      main: "#16a34a",
+    },
+    background: {
+      paper: "#F1F2F6",
+      default: "#ffffff",
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  typography: {
+    fontFamily: "Inter",
+    h3: {
+      fontWeight: "700",
+    },
+    h4: {
+      fontWeight: "700",
+    },
+    h5: {
+      fontWeight: "500",
+    },
+    body1: {
+      lineHeight: "1.8",
+    },
+    body2: {
+      lineHeight: "1.8",
+    },
+  },
+  mixins: {
+    toolbar: {
+      minHeight: "64px",
+    },
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "transparent",
+          boxShadow: "none",
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          boxShadow:
+            "0.5px 2px 3.1px rgba(0, 0, 0, 0.02), 4px 16px 25px rgba(0, 0, 0, 0.04)",
+        },
+      },
+    },
+
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: "600",
+        },
+      },
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+export default theme;
