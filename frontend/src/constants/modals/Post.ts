c10ce67f09649980ba57c1e198df0bf1527a5d6c
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2022-04-10 04:20:43 +0530

    feat: add post component

---
+++ frontend/src/constants/modals/Post.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  id: string;
+  created_at: Date;
+  loc_lan: number;
+  loc_lon: number;
+  user_id: string;
+  post_id: string;
+  post_url: string;
+  chat_id: string;
+  loc_name: string;
+  bucket_id: string;
+}
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,61 @@
 import React from "react";
 import { Grid, Box, Container } from "@mui/material";
+import { Post } from "../../components/Post/Post";
+import { Post as PostInterface } from "../../constants/modals/Post";
 
 interface DashboardProps {}
+
+const samplePosts: PostInterface[] = [
+  {
+    id: "1",
+    created_at: new Date(),
+    loc_lan: 12.731936,
+    loc_lon: 77.3305776,
+    user_id: "fsdfasd",
+    post_id: "1512792743914913797",
+    post_url: "https://twitter.com/AllThingTravel/status/1512792743914913797",
+    chat_id: "test chat id",
+    loc_name: "test location name",
+    bucket_id: "test bucket",
+  },
+  {
+    id: "2",
+    created_at: new Date(),
+    loc_lan: 12.731936,
+    loc_lon: 77.3305776,
+    user_id: "fsdfasd",
+    post_id: "1512792743914913797",
+    post_url: "https://twitter.com/AllThingTravel/status/1512792743914913797",
+    chat_id: "test chat id",
+    loc_name: "test location name",
+    bucket_id: "test bucket",
+  },
+  {
+    id: "3",
+    created_at: new Date(),
+    loc_lan: 12.731936,
+    loc_lon: 77.3305776,
+    user_id: "fsdfasd",
+    post_id: "1512792743914913797",
+    post_url: "https://twitter.com/AllThingTravel/status/1512792743914913797",
+    chat_id: "test chat id",
+    loc_name: "test location name",
+    bucket_id: "test bucket",
+  },
+];
 
 export const Dashboard: React.FC<DashboardProps> = () => {
   return (
     <>
       <Container>
         <h4>Dashboard</h4>
+        <Grid container spacing={3}>
+          {samplePosts.map((post) => (
+            <Grid item xs={12} key={post.id}>
+              <Post post={post} />
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </>
   );
