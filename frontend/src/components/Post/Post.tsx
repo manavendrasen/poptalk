2b9338749e6c1e65dd6087fdc4ae8fadf0f73dc7
Manavendra Sen <manavendra4288@gmail.com>
2022-04-10 15:51:29 +0530

    fix:  twitter embed

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-  Card,
-  Box,
-  Typography,
-  Button,
-  CircularProgress,
-} from "@mui/material";
+import { Card, Box, Typography, Button, CircularProgress } from "@mui/material";
+import { Tweet } from "react-twitter-widgets";
 import { Post as PostInterface } from "../../constants/modals/Post";
 import { useRecoilState } from "recoil";
 import locationState from "../../recoil/atoms/location";
@@ -18,6 +13,8 @@ interface PostProps {
 export const Post: React.FC<PostProps> = ({ bucketPost }) => {
   const [location, setLocation] = useRecoilState(locationState);
   const [post, setPost] = useRecoilState(postState);
+
+  console.log(post);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const openOptions = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -95,11 +92,35 @@ export const Post: React.FC<PostProps> = ({ bucketPost }) => {
         </Box>
       </Box>
 
-      <blockquote className='twitter-tweet'>
+      {/* <blockquote className='twitter-tweet'>
         <a href={bucketPost.post_url}>
           <CircularProgress />
         </a>
-      </blockquote>
+      </blockquote> */}
+
+      <Box
+        sx={{
+          width: "100%",
+          marginLeft: "4rem",
+          "& > *": {
+            width: "100%",
+          },
+        }}
+      >
+        <Tweet
+          tweetId={bucketPost.post_id}
+          renderError={(error) => {
+            console.log(error, bucketPost);
+            return <div>Error</div>;
+          }}
+        />
+      </Box>
+
+      {/* <TwitterTweetEmbed
+        onLoad={function noRefCheck() {}}
+        placeholder={<CircularProgress />}
+        tweetId={post.id}
+      /> */}
       {/* 
       <Menu
         id='basic-menu'
