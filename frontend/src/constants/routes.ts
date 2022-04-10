622b5c66c9c7ea16401208375cd4677f70435191
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2022-04-10 13:30:27 +0530

    Add: rtc routing

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -13,6 +13,5 @@ export const FAV = `${APP}/fav`;
 export const BUCKET = `${APP}/bucket`;
 export const BUCKET_DETAIL = `${BUCKET}/:id`;
 
-export const CHAT = `${APP}/chat`;
-
 export const POSTS_ROUTE = "/48d9b6ac-b732-49f6-83ac-b2e103bd1b50/bucket/1/post";
+export const CHAT = `${APP}/chat/:id`;
