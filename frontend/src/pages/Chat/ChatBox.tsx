3364dde884ce3196067e7149629bd33d293a7102
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2022-04-10 06:29:33 +0530

    Add: chat twitter auth

--- frontend/src/pages/Chat/ChatBox.tsx
+++ frontend/src/pages/Chat/ChatBox.tsx
@@ -3,18 +3,21 @@ import { PubNubProvider } from "pubnub-react";
 import React, { useState } from "react";
 import MessageClient from "../../MessageClient";
 import { Chat, MessageList, MessageInput, TypingIndicator } from "@pubnub/react-chat-components";
+import { supabase } from "../../supabaseClient";
 
 interface ChatBoxProps {}
 
-export const ChatBox: React.FC<ChatBoxProps> = ({}) => {
-  const [msgClient, setMsgClient] = useState<MessageClient>(() => {
-    return new MessageClient(Date.now().toString());
+export const ChatBox: React.FC<ChatBoxProps> = () => {
+  const [msgClient] = useState<MessageClient>(() => {
+    return new MessageClient(
+      supabase.auth.user()?.user_metadata.full_name
+    );
   });
 
   return (
     <Container>
       <PubNubProvider client={msgClient.pubnub}>
-        <Chat currentChannel={"Default"}>
+        <Chat currentChannel={"Default"} >
           <MessageList enableReactions>
             <TypingIndicator />
           </MessageList>
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { supabase } from "../../supabaseClient";
 
 interface HomeProps {}
 
-export const Home: React.FC<HomeProps> = ({}) => {
+export const Home: React.FC<HomeProps> = () => {
+  console.log(supabase.auth.user());
   return (
     <Container>
       <h1>Home</h1>
